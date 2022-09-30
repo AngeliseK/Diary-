f795ccd7384c5adbd570cfdf20e91779b3216194
AngeliseK <Angelisekaranja@icloud.com>
2022-09-30 15:56:30 +0100

    W1 & W2

--- Week1_and_Week2.rb
+++ Week1_and_Week2.rb
@@ -1,1 +1,35 @@
 Week 1 & Week 2
+
+=begin Week1: 
+
+- Focused on navigating the command line
+    - Creating files
+    - Listing hidden files
+    - Navigating through directories 
+- Focused on Git Version Control 
+    - Setting up Git 
+    - Initialising Git 
+    - Commiting files 
+
+- This part of the module was quite simple - I enjoyed learning about Git and how essential it is to software engineers. 
+- Committing files etc - also helped me with the ruby foundation challenges after 
+
+Week2:
+- Focused on Learning the foundations of Ruby 
+    - Stings
+    - Arguments 
+    - Conditionals
+    - Methods 
+    - Arrays 
+    - Classes 
+    - Hashes 
+    - Object Instantiation 
+
+- I found this part more challengeing, especially Chapter 2 and Chapter 3. 
+- The drills were very useful, and it was really fun going through them, and using the rspec. 
+- The chapter 2 programming, took me longer than I would have liked - I think object instantiation and fully understanding the relationship between classes/methods - is something I need to focus on. 
+=end 
+
+
+
+
