00279683c948274328e18aeac0456363789e48e2
AngeliseK <Angelisekaranja@icloud.com>
2022-10-17 15:15:21 +0100

    module two

---
+++ Week3_and_Week4.rb
@@ -0,0 +1,41 @@
+=begin
+
+WEEK 3: 
+Focused on 
+    - Testing methods and classes 
+    - Testing for errors 
+    - Designing classes and then tested them 
+
+- This first part of the module I struggled with - However I realised the importance of diagrams.\
+- I began to draw using excali draw my classes - so that I understood the relationship between the multi-class-systems 
+- Also, creating my own tests allowed me to understand classes and methods more 
+- I went back to ruby foundations and did the RSPEC for some of the drills (so that I could practise more)
+- I think I need to focus on the multi-class-system part more, so that I am more 
+    familiar and confident with different ruby functions and writing code to get the output I want - this is definitely an area I want to improve on 
+- I received good feedback from my coach Will on one of the challenges - I followed 
+    the TDD process well 
+
+
+WEEK 4:
+Focused on:
+    - Unit Tests 
+    - Peer class testing 
+    - Doubles 
+    - API 
+    - Terminal IO 
+    - Solo Project 
+
+- This week was more easier for me. I think it's because I switched up my learning style. 
+- I found the doubles, unit testing, terminal IO quite easy to follow and understand 
+- I particularly enjoyed the solo project - I think unit testing and doubles allowed me to understand 
+    multi-class-systems better. 
+
+
+Need To Work On:
+- Week 3's content - in particular the Multi-class-system challenge 
+
+
+
+
+
+=end 
